8e0ee4566acbf5e4fc9772b0d8b40b397bb5a60e
Kumar Aryan Subudhi <kumar.subudhi123@gmail.com>
2025-09-16 19:12:53 +0530

    testimonials updated

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg'
 import logo_icon from './logo_icon.svg'
-import facebook_icon from './facebook_icon.svg'
+import facebook_icon from './githublogo.png'
 import instagram_icon from './instagram_icon.svg'
 import twitter_icon from './twitter_icon.svg'
 import star_icon from './star_icon.svg'
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,9 +7,17 @@ const Footer = () => {
         <img width={150} src={assets.logo} alt="" />
         <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Aryan | All right reserved.</p>
         <div className='flex gap-2.5'>
-            <img width={35} src={assets.facebook_icon} alt="" />
-            <img width={35} src={assets.twitter_icon} alt="" />
-            <img width={35} src={assets.instagram_icon} alt="" />
+            <img width={35} src={assets.facebook_icon} alt="" 
+            style={{cursor:"pointer"}}
+            onClick={()=>{
+              window.location.href="https://github.com/kumar-aryan-s"
+            }}/>
+        
+            <img width={35} src={assets.instagram_icon} alt="" 
+            style={{cursor:"pointer"}}
+            onClick={()=>{
+              window.location.href="https://www.instagram.com/kumar_aryan_s/?next=%2F"
+            }}/>
         </div>
     </div>
   )
